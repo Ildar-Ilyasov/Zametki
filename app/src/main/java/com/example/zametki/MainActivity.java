8a543ed95a4005fac47340628474f1e12123ee4f
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-11-01 12:23:08 +0300

    init

--- app/src/main/java/com/example/zametki/MainActivity.java
+++ app/src/main/java/com/example/zametki/MainActivity.java
@@ -21,6 +21,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    // Предопределяем таблицы с темами и текстом
     public String[] NotesThemes = {"Тема 1","Тема 2","Тема 3","Тема 4","Тема 5",};
     public String[] NotesText = {"dsfsdfsdf 1","sfadfsdvsd 2","sdfsdfsdf 3","sdfsdfsdf 4","dsfsdfds 5",};
 
@@ -29,17 +30,21 @@ public class MainActivity extends AppCompatActivity {
         NotesText[pos] = text;
     }
 
+    // получение результата
     ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
 
                     if(result.getResultCode() == Activity.RESULT_OK){
+                        // получаем интент и данные из него
                         Intent intent = result.getData();
                         int pos = intent.getIntExtra("pos", 0);
                         String Theme = intent.getStringExtra("Theme");
                         String Text = intent.getStringExtra("Text");
+                        // дебаг
                         Toast.makeText(MainActivity.this, Theme + " | " + Text, Toast.LENGTH_SHORT).show();
+                        // выставялем новые данные
                         NotesThemes[pos] = Theme;
                         NotesText[pos] = Text;
 
@@ -67,11 +72,14 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Получаем позицию, создаем интент
                 String selectedItem = (String) parent.getItemAtPosition(position);
                 Intent intent = new Intent(MainActivity.this, Note.class);
+                // вставляем данные
                 intent.putExtra("Theme", NotesThemes[position]);
                 intent.putExtra("Text", NotesText[position]);
                 intent.putExtra("pos", position);
+                // запускаем
                 mStartForResult.launch(intent);
                 //startActivity(intent);
             }
--- app/src/main/java/com/example/zametki/Note.java
+++ app/src/main/java/com/example/zametki/Note.java
@@ -15,30 +15,38 @@ public class Note extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note);
+        // получение объектов текста
         TextView EditText = findViewById(R.id.editText);
         TextView EditText2 = findViewById(R.id.editText2);
+        // получение переданого интента
         Intent intent = getIntent();
         String Theme = intent.getStringExtra("Theme");
         String Text = intent.getStringExtra("Text");
         int pos = intent.getIntExtra("pos", 0);
+        // получение объекта кнопки
         Button backButton = findViewById(R.id.backbutton);
+        // слушаем ивент
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // создание интента
                 Intent data = new Intent();
+                // всталвяем данные
                 data.putExtra("Theme", EditText.getText());
                 data.putExtra("Text", EditText2.getText());
                 data.putExtra("pos", pos);
-                Toast.makeText(Note.this, EditText.getText() + " | " + EditText2.getText(), Toast.LENGTH_SHORT).show();
+                // дебаг
+                Toast.makeText(Note.this, data.getStringExtra("Theme") + " | " + EditText2.getText(), Toast.LENGTH_SHORT).show();
+                // выставление результат
                 setResult(RESULT_OK, data);
+                // завершение работы активности
                 finish();
             }
         });
 
 
 
-
+        // выставляем получение данные
         EditText.setText(Theme);
         EditText2.setText(Text);
 
